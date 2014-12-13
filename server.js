dacc2803c9eeda6195b10d77bf846f977bdfdbee
wojjas <wojtek@proxoft.se>
2014-12-13 17:44:32 +0200

    Adding Stylus compleate

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 var express = require('express');
-var jade = require('jade');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+//var jade = require('jade');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
 //Configure express:
-console.log('dbg1: ' + __dirname);
-app.set('views', './server/views');
-//app.set('views', __dirname + '/server/views');
+//app.set('views', './server/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
 
 //Redirects to index and lets the client-side-routing route depending on what's appended to index
 app.get('*', function (req, res) {
