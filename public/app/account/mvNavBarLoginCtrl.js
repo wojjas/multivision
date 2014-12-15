2b34e87c85ecf55f22fad29f711fbeed9c20fa05
wojjas <wojtek@proxoft.se>
2014-12-15 14:45:41 +0200

    Adding the Login Display, compleated

---
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('mvNavBarLoginCtrl', function ($scope) {
+            $scope.signin = function (username, password) {
+                console.log("I'm not done yet");
+            }
+        })
+})();
--- server.js
+++ server.js
@@ -61,8 +61,8 @@ db.once('open', function callback(){
 //})
 
 
-app.get('/partials/:partialPath', function (req, res) {
-    res.render('partials/' + req.params.partialPath);
+app.get('/partials/*', function (req, res) {
+    res.render('../../public/app/' + req.params[0]);
 })
 
 //Redirects to index and lets the client-side-routing route depending on what's appended to index
