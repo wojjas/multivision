61e18cc036b068abf7cc4c1daf184b855ff750e0
wojjas <wojtek@proxoft.se>
2014-12-26 09:19:24 +0200

    Cennects to local db in dev

--- server/config/config.js
+++ server/config/config.js
@@ -3,14 +3,12 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
     dev: {
-        //db: 'mongodb://localhost/multivision',
-        db: 'mongodb://wojjas:test@ds063170.mongolab.com:63170/multivision',
+        db: 'mongodb://localhost/multivision',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
     prod: {
         rootPath: rootPath,
-        //db: 'mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision',
         db: 'mongodb://wojjas:test@ds063170.mongolab.com:63170/multivision',
         port: process.env.PORT || 80
     }
