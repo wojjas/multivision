31ec4e67b4117f2632d584b28122ac33d6cfc657
wojjas <wojtek@proxoft.se>
2014-12-26 09:11:51 +0200

    Connecting to proXoft's mongoLag db

--- server/config/config.js
+++ server/config/config.js
@@ -3,13 +3,15 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
     dev: {
-        db: 'mongodb://localhost/multivision',
+        //db: 'mongodb://localhost/multivision',
+        db: 'mongodb://wojjas:test@ds063170.mongolab.com:63170/multivision',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
     prod: {
         rootPath: rootPath,
-        db: 'mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision',
+        //db: 'mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision',
+        db: 'mongodb://wojjas:test@ds063170.mongolab.com:63170/multivision',
         port: process.env.PORT || 80
     }
 }
