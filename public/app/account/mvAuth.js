da080907496888c557e4c23895f1a6d370ca2851
wojjas <wojtek@proxoft.se>
2014-12-26 08:02:31 +0200

    Improving the Client Login Code, compleated

---
+++ public/app/account/mvAuth.js
@@ -0,0 +1,18 @@
+angular.module('app').factory('mvAuth', function ($http, $q, mvIdentity) {
+    return{
+        authenticateUser: function (username, password) {
+            var dfd = $q.defer();
+            $http.post('/login', {username:username, password:password})
+                .then(function (response) {
+                    if(response.data.success){
+                        mvIdentity.currentUser = response.data.user;
+                        dfd.resolve(true);
+                    }
+                    else{
+                        dfd.resolve(false);
+                    }
+                });
+            return dfd.promise;
+        }
+    }
+});
--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,18 +1,19 @@
 (function () {
     'use strict';
 
-    angular.module('app')
-        .controller('mvNavBarLoginCtrl', function ($scope, $http) {
+    angular
+        .module('app')
+        .controller('mvNavBarLoginCtrl', function ($scope, $http, mvNotifier, mvIdentity, mvAuth) {
+            $scope.identity = mvIdentity;
             $scope.signin = function (username, password) {
-                $http.post('/login', {username:username, password:password})
-                    .then(function (response) {
-                        if(response.data.success){
-                            console.log('logged in!');
-                        }
-                        else{
-                            console.log('failed to log in!');
-                        }
-                    })
+                mvAuth.authenticateUser(username, password).then(function (success) {
+                    if(success){
+                        mvNotifier.notify('You have successfully signed in!');
+                    }
+                    else{
+                        mvNotifier.notify('Username/Password combination incorrect!');
+                    }
+                });
             }
-        })
+        });
 })();
